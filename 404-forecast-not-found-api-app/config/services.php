bf68d9a7b36c2350a580af27007bf10da38cbd6e
Lumu Kabongo <lumu.kabongo@superbalist.com>
2025-06-13 14:49:03 +0200

    added city migration, adn city list extraction command

--- 404-forecast-not-found-api-app/config/services.php
+++ 404-forecast-not-found-api-app/config/services.php
@@ -14,6 +14,7 @@ return [
     |
     */
     'openweathermap' => [
+        'geo_location_url' => env('OPENWEATHERMAP_GEO_BASE_URL', 'https://api.openweathermap.org/geo/1.0/direct'),
         'api_key' => env('OPENWEATHERMAP_API_KEY'),
         'base_url' => env('OPENWEATHERMAP_BASE_URL', 'https://api.openweathermap.org/data/2.5'),
     ],
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 404-forecast-not-found-project
+
+
+
+
+#Commands
+php artisan app:import-za-cities //imports a list of cities from za-cities.json provided by default weather provider
